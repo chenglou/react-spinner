49f31e70f0fed409c6867e61c163e44216210caa
Cheng Lou <chenglou92@gmail.com>
2013-09-07 12:18:45 -0400

    done, zipping soon

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ Or simply drop the script somewhere on your page (after React of course):
 <script src="path/to/react-spinner.js"></script>
 ```
 
+And the CSS file:
+
+```html
+<link rel="stylesheet" type="text/css" href="path/to/react-spinner.css">
+```
+
 ## Usage
 
 ```html
@@ -21,7 +27,7 @@ Or simply drop the script somewhere on your page (after React of course):
 * @jsx React.DOM
 */
 
-// it doesn't get simpler than this
+// doesn't get simpler than this, does it?
 React.renderComponent(<Spinner/>, document.body);
 ```
 
---
+++ react-spinner.js
@@ -0,0 +1,27 @@
+var Spinner = React.createClass({
+  render: function() {
+    var bars = [];
+    var barStyle;
+
+    for (var i = 0; i < 12; i++) {
+      barStyle = {};
+      barStyle.WebkitAnimationDelay =
+        barStyle.animationDelay = (i - 12) / 10 + 's';
+
+      barStyle.WebkitTransform =
+        barStyle.transform = 'rotate(' + (i * 30) + 'deg) translate(9.5px)';
+
+      bars.push(
+        React.DOM.div({style: barStyle, className: "loading-spinner-bar"})
+      );
+    }
+
+    return (
+      React.DOM.div({className: "loading-spinner"},
+        React.DOM.div({className: "loading-spinner-inner"},
+          bars
+        )
+      )
+    );
+  }
+});
