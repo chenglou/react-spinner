27adec4f1f95c0b03c70d6530b2266687b56c279
Cheng Lou <chenglou92@gmail.com>
2013-09-07 11:58:07 -0400

    add big option

--- react-spinner.jsx
+++ react-spinner.jsx
@@ -2,43 +2,25 @@
 * @jsx React.DOM
 */
 
-function shallowClone(obj) {
-  var returnObj = {};
-  for (var key in obj) {
-    if (!obj.hasOwnProperty(key)) continue;
-    returnObj[key] = obj[key];
-  }
-  return returnObj;
-}
-
 var Spinner = React.createClass({
   render: function() {
     var bars = [];
-    var barWidth = 13;
-    var barHeight = 4;
+    var big = this.props.big;
+    var translateFromCenter = 9.5 * (big ? 2 : 1);
 
-    var generalStyle = {
-      width: barWidth,
-      height: barHeight,
-      top: -barHeight / 2,
-      left: -barWidth / 2,
-    };
-    var specificStyle;
-    var barColor;
+    var barStyle;
+    var barClass = 'loading-spinner-bar' + (big ? ' loading-spinner-bar-big' : '');
 
     for (var i = 0; i < 12; i++) {
-      specificStyle = shallowClone(generalStyle);
-      specificStyle.WebkitAnimationDelay = specificStyle.animationDelay = (i - 12) / 10 + 's';
-      specificStyle.WebkitTransform = specificStyle.transform = 'rotate(' + (i * 30) + 'deg) translate(19px)';
-      bars.push(<div style={specificStyle} className="loading-spinner-bar"/>);
+      barStyle = {};
+      barStyle.WebkitAnimationDelay = barStyle.animationDelay = (i - 12) / 10 + 's';
+      barStyle.WebkitTransform = barStyle.transform = 'rotate(' + (i * 30) + 'deg) translate(' + translateFromCenter + 'px)';
+      bars.push(<div style={barStyle} className={barClass}/>);
     }
 
-    var wrapperStyle = {
-      width: '64px',
-      height: '64px',
-    };
+    var spinnerClass = 'loading-spinner' + (big ? ' loading-spinner-big' : '');
     return (
-      <div style={wrapperStyle} className="loading-spinner"><div className="loading-spinner-inner">{bars}</div></div>
+      <div className={spinnerClass}><div className="loading-spinner-inner">{bars}</div></div>
     );
   }
 });
