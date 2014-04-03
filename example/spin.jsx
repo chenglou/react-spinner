5034619713108d7b32703c670adca66a85b7132c
Cheng Lou <chenglou92@gmail.com>
2014-04-02 18:55:56 -0700

    Update example for react component store

--- example/spin.jsx
+++ example/spin.jsx
@@ -1,10 +1,14 @@
 /** @jsx React.DOM */
 
+var Spinner = require('Spinner');
+var React = require('React');
+
 var App = React.createClass({
   render: function() {
     var style = {
       height: 50,
-      width: 50
+      width: 50,
+      backgroundColor: 'black'
     };
     return (
       <div style={style}>
