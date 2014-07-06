82e3d9ee8355a217f5e494b289f82ef0c7d4bbb9
Cheng Lou <chenglou92@gmail.com>
2014-07-06 15:46:34 -0700

    Bundle correctly for browserify

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /** @jsx React.DOM */
 
-var Spinner = React.createClass({displayName: 'Spinner',
-  render: function() {
-    var bars = [];
-    var barStyle;
+(function(window, React) {
+  var Spinner = React.createClass({displayName: 'Spinner',
+    render: function() {
+      var bars = [];
+      var barStyle;
 
-    for (var i = 0; i < 12; i++) {
-      barStyle = {};
-      barStyle.WebkitAnimationDelay = barStyle.animationDelay =
-        (i - 12) / 10 + 's';
+      for (var i = 0; i < 12; i++) {
+        barStyle = {};
+        barStyle.WebkitAnimationDelay = barStyle.animationDelay =
+          (i - 12) / 10 + 's';
 
-      barStyle.WebkitTransform = barStyle.transform =
-        'rotate(' + (i * 30) + 'deg) translate(146%)';
+        barStyle.WebkitTransform = barStyle.transform =
+          'rotate(' + (i * 30) + 'deg) translate(146%)';
 
-      bars.push(
-        React.DOM.div( {style:barStyle, className:"react-spinner_bar"} )
+        bars.push(
+          React.DOM.div( {style:barStyle, className:"react-spinner_bar"} )
+        );
+      }
+
+      return this.transferPropsTo(
+        React.DOM.div( {className:"react-spinner"}, bars)
       );
     }
+  });
 
-    return this.transferPropsTo(
-      React.DOM.div( {className:"react-spinner"}, bars)
-    );
+  if (typeof module === 'undefined') {
+    window.Spinner = Spinner;
+  } else {
+    module.exports = Spinner;
   }
-});
-
-if (typeof module === 'undefined') {
-  window.Spinner = Spinner;
-} else {
-  module.exports = Spinner;
-}
-
-},{}]},{},[1])
+})(window, typeof require === 'function' ? require('React') : React);
--- index.jsx
+++ index.jsx
@@ -1,31 +1,34 @@
 /** @jsx React.DOM */
 
-var Spinner = React.createClass({
-  render: function() {
-    var bars = [];
-    var barStyle;
+(function(window, React) {
+  var Spinner = React.createClass({
+    render: function() {
+      var bars = [];
+      var barStyle;
 
-    for (var i = 0; i < 12; i++) {
-      barStyle = {};
-      barStyle.WebkitAnimationDelay = barStyle.animationDelay =
-        (i - 12) / 10 + 's';
+      for (var i = 0; i < 12; i++) {
+        barStyle = {};
+        barStyle.WebkitAnimationDelay = barStyle.animationDelay =
+          (i - 12) / 10 + 's';
 
-      barStyle.WebkitTransform = barStyle.transform =
-        'rotate(' + (i * 30) + 'deg) translate(146%)';
+        barStyle.WebkitTransform = barStyle.transform =
+          'rotate(' + (i * 30) + 'deg) translate(146%)';
 
-      bars.push(
-        <div style={barStyle} className="react-spinner_bar" />
+        bars.push(
+          <div style={barStyle} className="react-spinner_bar" />
+        );
+      }
+
+      return this.transferPropsTo(
+        <div className="react-spinner">{bars}</div>
       );
     }
+  });
 
-    return this.transferPropsTo(
-      <div className="react-spinner">{bars}</div>
-    );
+  if (typeof module === 'undefined') {
+    window.Spinner = Spinner;
+  } else {
+    module.exports = Spinner;
   }
-});
+})(window, typeof require === 'function' ? require('React') : React);
 
-if (typeof module === 'undefined') {
-  window.Spinner = Spinner;
-} else {
-  module.exports = Spinner;
-}
