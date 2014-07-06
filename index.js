05518efe27bc585352efdd816e166addeba11359
Cheng Lou <chenglou92@gmail.com>
2014-07-06 14:54:46 -0700

    Adjust spinner initial rotation degree

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /** @jsx React.DOM */
 
 var Spinner = React.createClass({displayName: 'Spinner',
@@ -11,7 +12,7 @@ var Spinner = React.createClass({displayName: 'Spinner',
         (i - 12) / 10 + 's';
 
       barStyle.WebkitTransform = barStyle.transform =
-        'rotate(' + (i * 30) + 'deg) translate(-146%)';
+        'rotate(' + (i * 30) + 'deg) translate(146%)';
 
       bars.push(
         React.DOM.div( {style:barStyle, className:"react-spinner_bar"} )
@@ -29,3 +30,5 @@ if (typeof module === 'undefined') {
 } else {
   module.exports = Spinner;
 }
+
+},{}]},{},[1])
